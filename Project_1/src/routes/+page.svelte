3989e3b0d975ea66bdd8e5d9d411f691a07b065a
Paul Pfister <thi22p@hotmail.com>
2025-01-07 15:35:08 +0100

    New Functions

--- Project_1/src/routes/+page.svelte
+++ Project_1/src/routes/+page.svelte
@@ -2,3 +2,4 @@
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
 <p>Dieser Link führt zu ksso.so.ch</p>
 <a href="https://ksso.so.ch">ksso.so.ch</a>
+<p>New</p>
